bd2ead86e03af36971a561bd15775f17e27197af
Pavlo Didun <123100503+pavlodidun@users.noreply.github.com>
2023-04-19 06:19:41 +0000

    updated project

--- game.js
+++ game.js
@@ -1,98 +1,61 @@
-const message = document.querySelector('.container-game__selector-message');
-const selectTransport = document.querySelectorAll('.container-game__selector img');
-const button = document.querySelector('.start');
-const element = document.querySelector('.container-game__selector');
-const gamepad = document.querySelector('.container-game__gamepad');
+const items = document.querySelectorAll('.items img');
+const transport = document.querySelector('.item-in');
+const boat = document.querySelector('.boat');
+let selected = null;
 
-
-function checkActiveTransport() {
-    let isActiveTransport = false;
-    selectTransport.forEach(function (transportAdd) {
-        if (transportAdd.classList.contains('active')) {
-            isActiveTransport = true;
+items.forEach(item => {
+    item.addEventListener('click', () => {
+        if (selected === item) {
+            if (transport.contains(item)) {
+                transport.removeChild(item);
+                document.querySelector('.items').appendChild(item);
+            }
+            selected = null;
+            return;
         }
+
+        if (selected !== null) {
+            return;
+        }
+
+        transport.appendChild(item);
+        selected = item;
     });
-    return isActiveTransport;
-}
+});
 
-button.addEventListener('click', handleButtonClick);
+// Movement
+boat.addEventListener('click', () => {
+    if (boat.classList.contains('move-boat')) {
+        boat.classList.remove('move-boat');
+        boat.classList.add('move-reverse-boat');
+        transport.classList.remove('move-item');
+        transport.classList.add('move-reverse-item');
+    } else {
+        boat.classList.remove('move-reverse-boat');
+        boat.classList.add('move-boat');
+        transport.classList.remove('move-reverse-item');
+        transport.classList.add('move-item');
+    }
+});
 
-selectTransport.forEach(function (transportAdd) {
-    transportAdd.addEventListener('click', function () {
-        selectTransport.forEach(function (transportRemove) {
-            transportRemove.classList.remove('active');
-            transportRemove.classList.add('inactive');
+// left side
+const itemInImg = document.querySelector('.item-in img');
+const div3 = document.querySelector('#div3');
+
+setInterval(() => {
+    const itemInImg = document.querySelector('.item-in img');
+    if (itemInImg) {
+        itemInImg.addEventListener('click', () => {
+            if (boat.classList.contains('move-boat')) {
+                div3.appendChild(itemInImg);
+            }
         });
-        transportAdd.classList.add('active');
-        transportAdd.classList.remove('inactive');
-    });
-});
-
-function handleButtonClick() {
-    if (checkActiveTransport()) {
-        element.classList.add('swith-off');
-        button.classList.add('swith-off');
-        gamepad.classList.add('swith-on');
-    } else {
-        message.style.display = 'block';
     }
-}
-// Timer
-// function getActiveTransport() {
-//     let activeTransport = '';
-//     selectTransport.forEach(function (transportAdd) {
-//         if (transportAdd.classList.contains('active')) {
-//             activeTransport = transportAdd.alt;
-//         }
-//     });
-//     return activeTransport;
-// }
+}, 1000);
 
-// function handleButtonClick() {
-//     const activeTransport = getActiveTransport();
-//     if (activeTransport !== '') {
-//         element.classList.add('swith-off');
-//         gamepad.classList.add('swith-on');
-
-//         if (activeTransport === 'Airship') {
-//             document.querySelector('.container-game__airship').classList.add('selected-airship');
-//         } else if (activeTransport === 'Boat') {
-//             document.querySelector('.container-game__boat').classList.add('selected-boat');
-//         }
-//     } else {
-//         message.style.display = 'block';
-//     }
-// }
-
-// Animation
-// const myDiv = document.getElementById('boat-block');
-// // myDiv.addEventListener('click', () => {
-// //     myDiv.classList.add('myAnimation');
-// // });
-
-// myDiv.addEventListener('click', () => {
-//     if (myDiv.classList.contains('myAnimation')) {
-//         myDiv.classList.remove('myAnimation');
-//     } else {
-//         myDiv.classList.add('myAnimation');
-//     }
-// });
-
-const myDiv = document.getElementById('boat-block');
-
-myDiv.addEventListener('click', () => {
-    if (myDiv.classList.contains('myAnimation')) {
-        myDiv.classList.remove('myAnimation');
-        myDiv.classList.add('myReverseAnimation');
-    } else {
-        myDiv.classList.remove('myReverseAnimation');
-        myDiv.classList.add('myAnimation');
+div3.addEventListener('click', (event) => {
+    const image = event.target.closest('img');
+    if (image && boat.classList.contains('move-boat')) {
+        transport.appendChild(image);
     }
-});
-
-// Coordinates
-document.addEventListener('click', (event) => {
-    const x = event.clientX;
-    const y = event.clientY;
-    console.log(`Clicked at (${x}, ${y})`);
 });
